d8e3c2e857f1c502082f2e70bcd4a50d6286533b
Eduardo Barbosa <eduardobarbosalhp@gmail.com>
2024-05-11 20:56:30 -0300

    feat: inserção de imagem padrão, camera e galeria

--- app/src/main/java/com/example/melitruko/presentation/ui/view/fragments/NewPlayerFragment.java
+++ app/src/main/java/com/example/melitruko/presentation/ui/view/fragments/NewPlayerFragment.java
@@ -7,11 +7,9 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.ImageDecoder;
 import android.graphics.drawable.ColorDrawable;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +19,6 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -37,22 +34,20 @@ public class NewPlayerFragment extends DialogFragment {
 
     private FragmentNewPlayerBinding binding;
     private HomeViewModel viewModel;
+    private static final String CAMERA_PERMISSION = Manifest.permission.CAMERA;
     private static final String GALLERY_PERMISSION = Manifest.permission.READ_EXTERNAL_STORAGE;
+    private ActivityResultLauncher<String> requestPermissionCamera;
     private ActivityResultLauncher<String> requestPermissionGallery;
+    private ActivityResultLauncher<Intent> resultCamera;
     private ActivityResultLauncher<Intent> resultGallery;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        createImageBitmap();
-
-        requestPermissionGallery = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-            if (isGranted){
-                resultGallery.launch(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI));
-            } else {
-                Toast.makeText(requireContext(), "É preciso permitir o acesso à galeria", Toast.LENGTH_LONG).show();
-            }
-        });
+        createImageOfGallery();
+        createImageOfCamera();
+        requestPermissionGallery();
+        requestPermissionCamera();
     }
 
     @Override
@@ -63,11 +58,11 @@ public class NewPlayerFragment extends DialogFragment {
         this.getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
         binding.btnDefaultImage.setOnClickListener(view -> {
-            binding.ivPlayer.setImageResource(R.drawable.default_person_bmp);
+            createBitmapDefaultImage();
         });
 
         binding.btnCamera.setOnClickListener(view -> {
-
+            validationCameraPermission();
         });
 
         binding.btnGallery.setOnClickListener(view -> {
@@ -75,7 +70,7 @@ public class NewPlayerFragment extends DialogFragment {
         });
 
         binding.ibRemovePhoto.setOnClickListener(view -> {
-
+            removePhoto();
         });
 
         binding.btnCancel.setOnClickListener(view -> dismiss());
@@ -83,15 +78,33 @@ public class NewPlayerFragment extends DialogFragment {
         return binding.getRoot();
     }
 
-    private void validationGalleryPermission() {
-        if (isGalleryPermissionGranted()) {
-            resultGallery.launch(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI));
-        } else {
-            requestPermissionGallery.launch(GALLERY_PERMISSION);
+    private void createBitmapDefaultImage() {
+        Bitmap bitmap = null;
+        ImageDecoder.Source source;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            source = ImageDecoder.createSource(requireActivity().getResources(), R.drawable.default_person_bmp);
+
+            try {
+                bitmap = ImageDecoder.decodeBitmap(source);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
+        setupPlayerImageView(bitmap);
     }
 
-    private void createImageBitmap() {
+    private void createImageOfCamera() {
+        resultCamera = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            Bitmap bitmap = null;
+            if ((result.getData() != null) && (result.getData().getExtras() != null) && (result.getData().getExtras().get("data") != null)) {
+                Bundle extras = result.getData().getExtras();
+                bitmap = (Bitmap) extras.get("data");
+            }
+            setupPlayerImageView(bitmap);
+        });
+    }
+
+    private void createImageOfGallery() {
         resultGallery = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
             Bitmap bitmap = null;
             if ((result.getData() != null) && (result.getData().getData() != null)) {
@@ -116,18 +129,67 @@ public class NewPlayerFragment extends DialogFragment {
         });
     }
 
+    private void requestPermissionCamera() {
+        requestPermissionCamera = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            if (isGranted) {
+                resultCamera.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+            } else {
+                Toast.makeText(requireContext(), "É preciso permitir o acesso à câmera", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void requestPermissionGallery() {
+        requestPermissionGallery = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            if (isGranted) {
+                resultGallery.launch(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI));
+            } else {
+                Toast.makeText(requireContext(), "É preciso permitir o acesso à galeria", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void validationCameraPermission() {
+        if (isCameraPermissionGranted()) {
+            resultCamera.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+        } else {
+            requestPermissionCamera.launch(CAMERA_PERMISSION);
+        }
+    }
+
+    private void validationGalleryPermission() {
+        if (isGalleryPermissionGranted()) {
+            resultGallery.launch(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI));
+        } else {
+            requestPermissionGallery.launch(GALLERY_PERMISSION);
+        }
+    }
+
+    private boolean isCameraPermissionGranted() {
+        return ContextCompat.checkSelfPermission(requireContext(), NewPlayerFragment.CAMERA_PERMISSION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private boolean isGalleryPermissionGranted() {
+        return ContextCompat.checkSelfPermission(requireContext(), NewPlayerFragment.GALLERY_PERMISSION) == PackageManager.PERMISSION_GRANTED;
+    }
+
     private void setupPlayerImageView(Bitmap bitmap) {
         if (bitmap != null) {
+            binding.ivPlayer.setImageBitmap(bitmap);
             binding.ivPlayer.setVisibility(View.VISIBLE);
             binding.ibRemovePhoto.setVisibility(View.VISIBLE);
-            binding.ivPlayer.setImageBitmap(bitmap);
             binding.btnGallery.setVisibility(View.GONE);
             binding.btnCamera.setVisibility(View.GONE);
             binding.btnDefaultImage.setVisibility(View.GONE);
         }
     }
 
-    private boolean isGalleryPermissionGranted() {
-        return ContextCompat.checkSelfPermission(requireContext(), NewPlayerFragment.GALLERY_PERMISSION) == PackageManager.PERMISSION_GRANTED;
+    private void removePhoto() {
+        // TODO remover foto da view model
+        binding.ivPlayer.setVisibility(View.GONE);
+        binding.ibRemovePhoto.setVisibility(View.GONE);
+        binding.btnGallery.setVisibility(View.VISIBLE);
+        binding.btnCamera.setVisibility(View.VISIBLE);
+        binding.btnDefaultImage.setVisibility(View.VISIBLE);
     }
 }
